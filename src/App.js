a5df008868c87933c47b887b2b53d2881abfc78d
Noah Duminil <noahduminil6@gmail.com>
2023-01-25 13:25:21 +0100

    mise à jour de la page d'accueil

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import './sass/main.scss';
 import TypingAnimation from './components/TypingAnim';
 
 function App() {
   return (
     <div className="App">
       <div className="center">
-      <h1>&lt; Noah Duminil &#47;&gt;</h1>
-      <h2><TypingAnimation text="Bienvenue sur mon portfolio !"/></h2>
+        <h1>&lt; Noah Duminil &#47;&gt;</h1>
+        <h2><TypingAnimation text="Bienvenue sur mon portfolio !" /></h2>
+        <button className='enter'>Entrer dans mon monde</button>
       </div>
     </div>
   );
