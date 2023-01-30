7c8f78a8ac1ee6d89f6b41bd8040bc3e07bae7e1
Noah Duminil <noahduminil6@gmail.com>
2023-01-30 09:37:05 +0100

    creation du state de la section project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
+import Cars3000 from '../img/Cars3000.png'
+import PizzaOrder from '../img/PizzaOrder.png'
+import Drink3000 from '../img/Drink3000.png'
+import Movieshuffle from '../img/Movieshuffle.png'
 
 const Project = () => {
+  const [project, setProject] = useState (
+    {
+      titre : "Cars3000",
+      image : {Cars3000}
+    },
+    {
+      titre : "PizzaOrder",
+      image : {PizzaOrder}
+    },
+    {
+      titre : "Drink3000",
+      image : {Drink3000}
+    },
+    {
+      titre : "Movieshuffle",
+      image : {Movieshuffle}
+    }
+  )
   return (
-    <div>Project</div>
+    <div className='container'>
+      <Header/>
+      <div className="element">
+        <h2>Titre</h2>
+        <button className='details'>Détails</button>
+      </div>
+    </div>
   )
 }
 
