9b0b094f294a6eb6d54cffd5b026e99527515162
NoahDum <noahduminil6@gmail.com>
2023-11-06 16:38:05 +0100

    correction des bug affichage mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className="icon">
-        <a href="https://www.linkedin.com/in/noah-duminil-85431a220" target="_blank"><FaLinkedinIn size={35} /></a>
-        <a href="mailto:dumweb.solutions@gmail.com" target="_blank"><FiMail size={35} /></a>
-        <a href="https://github.com/NoahDum" target="_blank"><FiGithub size={35} /></a>
+        <a href="https://www.linkedin.com/in/noah-duminil-85431a220" target="_blank"><FaLinkedinIn size={20} /></a>
+        <a href="mailto:dumweb.solutions@gmail.com" target="_blank"><FiMail size={20} /></a>
+        <a href="https://github.com/NoahDum" target="_blank"><FiGithub size={20} /></a>
       </div>
     </div>
   )
